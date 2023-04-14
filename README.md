d7f7fdcb9efc71a7c0b32c43d0a565337f92893d
gqwer <128947759+gqwer0123@users.noreply.github.com>
2023-04-14 03:29:50 +0000

    commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # doit
  jump-to-python
+
+
+문자열 포맷 코드
+코드 설명
+%s 문자열(String)
+%c 문자 1개(character)
+%d 정수(Integer)
+%f 부동소수(floating-point)
+%o 8진수
+%x 16진수
+%% Literal %(문자 % 자체)s
--- String/String.py
+++ String/String.py
@@ -122,3 +122,112 @@ a[19:-7]
 # 'You need'
 
 ### 슬라이싱으로 문자열 나누기
+
+a = "20010331Rainy"
+date = a[:8]
+weather = a[8:]
+date
+# '20010331'
+weather
+# 'Rainy'
+
+### "Pithon"이라는 문자열을 "Python"으로 바꾸려면?
+a = "Pithon"
+a[1]
+# 'i'
+a [1] = 'y'
+
+"""
+
+오류가 발생한다. 왜냐하면 문자열의 요솟값은 바꿀 수 있는 값이 아니기 때문이다.
+문자열 자료형은 그 요솟값을 변경할 수 없다.
+(immutable한 자료형이라고도 부른다.)
+
+"""
+
+a = "Pithon"
+a[:1]
+# 'P'
+a[2:]
+# 'thon'
+a[:1] + 'y' + a[2:]
+# 'Python'
+
+
+### 문자열 포매팅
+## 문자열 포매팅(Formatting)이 있다.
+
+## "현재 온도는 18도입니다."
+## "현재 온도는 20도입니다."
+
+## 위 두 문자열은 모두 같은데 숫자만 다르다. 이렇게 문자열 안의 특정한 값을 바꿔야 할 경우가  있을 때 문자열 포매팅 기법을 이용한다.
+
+### 문자열 포매팅 따라 하기
+
+## 1.숫자 바로 대입
+
+"I eat %d apples." % 3
+# 'I eat 3 apples.'
+
+## 여기에서 %d는 문자열 포맷 코드라고 부른다.
+
+## 2. 문자열 바로 대입
+
+" I eat %s apples." %"five"
+# 'I eat five apples.'
+
+## 문자열을 대입할 때는 앞에서 배운 것처럼 큰따옴표나 작은따옴표를 반드시 써주어야 한다.
+
+## 3.숫자 값을 나타내는 변수로 대입
+number = 3
+"I eat %d apples." % number
+# 'I eat 3 apples.'
+
+## 4.2개 이상의 값 넣기
+number = 10
+day = "three"
+"I ate %d apples. so I was sick for %s days." % (number, day)
+# 'I ate 10 apples. so I was sick for three days.'
+
+## 위 예문처럼 2개 이상의 값을 넣으려면 마지막 % 다음 괄호 안에 콤마(,)로 구분하여 각각의 값을 넣어준다.
+
+## %s 포맷 코드 - 어떤 형태의 값이든 변환해 넣을 수 있다.
+
+### [포매팅 연산자 %d와 %를 같이 쓸 때는 %%를 쓴다.]
+
+## 문자열 포맷 코드인 %d와 %가 같은 문자열 안에 존재하는 경우, %를 나타내려면 반드시 %%로 사용하는 법칙이 있다.
+"Error is %d%%" % 98
+#'Error is 98%.'
+
+### 포맷 코드와 숫자 함께 사용하기
+## 1. 정렬과 공백
+"%10s" % "hi"
+# '          hi'
+
+## 앞의 예문에서 %10는 전체 길이가 10개인 문자열 공간에서 대입되는 값을 오른쪽으로 정렬하고 그 앞의 나머지는 공백으로 남겨 두라는 의미이다.
+
+## 2.소수점 표현하기
+"%0.4f" % 3.42134234
+# '3.4213'
+
+### format 함수를 사용한 포매팅
+## 문자열의 format 함수를 사용하면 좀 더 발전된 스타일로 문자열 포맷을 지정할 수 있다.
+
+## 숫자 바로 대입하기
+"I eat {0} apples.".format(3)
+'I eat 3 apples'
+
+## 문자열 바로 대입하기
+"I eat {0} apples".format("five")
+'I eat five apples'
+
+## 2개 이상의 값 넣기
+
+number = 10
+day = "three"
+
+"I ate {0} apples. so I was sick for {1} days.".format(number, day)
+
+#'I ate 10 apples. so I was sick for three days.'
+
+## 이름으로 넣기
